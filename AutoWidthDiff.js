fb7ef439e08dddd4b27b514d0166b476f38f26f9
Michael Barany <barany@snap-interactive.com>
2012-11-13 12:38:41 -0500

    Add AutoWidthDiff

---
+++ AutoWidthDiff.js
@@ -0,0 +1,5 @@
+(function(){
+	$(".context-loader-container").css({"width":"auto","margin":"0 5px 0 30px"});
+	$("#slider .frames").width("auto");
+	$("#slider .frames .frame").width("100%");
+})();
